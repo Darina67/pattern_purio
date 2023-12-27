d9ffec19c90e2ec6d55c31e870e3409f504502b6
Darina67 <user.email=75675517+Darina67@users.noreply.github.com>
2023-12-27 12:24:46 +0300

    upd

--- js/filter.js
+++ js/filter.js
@@ -1,271 +1,43 @@
-var years = [
-    "За всё время", 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023
-];
-
-var initialCategory = "Все области";
-var initialYear = years[0];
-var isProjectsSectionVisible = true;
-console.log(initialYear);
-
-function activateInitialButtons() {
-    $('.control-btn-disease').removeClass('active');
-    $('.control-btn-disease:first-child').addClass('active');
-
-    $('.control-btn-years').removeClass('active');
-    $('.control-btn-years:first-child').addClass('active');
-}
-
-function populateRadioButtonsForDiseases(diseases) {
-    var container = $('.disease-buttons');
-    container.empty();
-
-    $.each(diseases, function (index, disease) {
-        container.append($(
-            `<label class="control-btn-disease control-btn ${index === 0 ? 'active' : ''}"> 
-                ${disease.name}
-               <input ${index === 0 ? 'checked' : ''} type="radio" name="diseaseRadio" value="${disease.name}" />
-            </label>`
-        ));
-    });
-}
-
-function populateRadioButtonsForYears(diseases, selectedDisease) {
-    var container = $('.year-buttons');
-    container.empty();
-
-    // Add "За все время" button for the selected disease
-    container.append($(
-        `<label class="control-btn control-btn-years ${selectedDisease === initialCategory ? 'active' : ''}"> 
+var years=["За всё время",2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023],initialCategory="Все области",initialYear=years[0],isProjectsSectionVisible=!0;function activateInitialButtons(){$(".control-btn-disease").removeClass("active"),$(".control-btn-disease:first-child").addClass("active"),$(".control-btn-years").removeClass("active"),$(".control-btn-years:first-child").addClass("active")}function populateRadioButtonsForDiseases(e){var a=$(".disease-buttons");a.empty(),$.each(e,function(e,t){a.append($(`<label class="control-btn-disease control-btn ${0===e?"active":""}"> 
+                ${t.name}
+               <input ${0===e?"checked":""} type="radio" name="diseaseRadio" value="${t.name}" />
+            </label>`))})}function populateRadioButtonsForYears(e,a){var t=$(".year-buttons");if(t.empty(),t.append($(`<label class="control-btn control-btn-years ${a===initialCategory?"active":""}"> 
             За всё время
-            <input ${selectedDisease === initialCategory ? 'checked' : ''} type="radio" name="yearRadio" value="За всё время" data-disease="${selectedDisease}" />
-        </label>`
-    ));
-
-    if (selectedDisease === initialCategory) {
-        // If "Все области" is selected, display all years
-        $.each(years.slice(1), function (index, year) {
-            container.append($(
-                `<label class="control-btn control-btn-years"> 
-                    ${year}
-                   <input type="radio" name="yearRadio" value="${year}" />
-                </label>`
-            ));
-        });
-    } else {
-        // Display years based on the selected disease
-        var selectedDiseaseData = diseases.find(function (disease) {
-            return disease.name === selectedDisease;
-        });
-
-        var availableYears = selectedDiseaseData ? Object.keys(selectedDiseaseData.phases).map(Number) : [];
-
-        $.each(availableYears, function (index, year) {
-            container.append($(
-                `<label class="control-btn control-btn-years"> 
-                    ${year}
-                   <input type="radio" name="yearRadio" value="${year}" />
-                </label>`
-            ));
-        });
-    }
-}
-
-function update(selectedDisease, selectedYear) {
-    if (selectedYear === undefined) {
-        selectedYear = years[0];
-    }
-         // Удалите текущий класс "active" у всех кнопок годов
-         $('.controls.year-buttons .control-btn-years').removeClass('active');
-
-         // Найдите и добавьте класс "active" для кнопки года, соответствующей selectedYear
-         $(`.controls.year-buttons .control-btn-years input[value="${selectedYear}"]`).parent().addClass('active');
-
-    $('.projects-container').html('');
-
-    // Find the selected disease data in file1.js
-    var selectedDiseaseData = file1.find(function (item) {
-        return item.name === selectedDisease;
-    });
-
-    // If "За все время" is selected, display blocks based on file1.js data
-    if (selectedYear === "За всё время" && selectedDiseaseData) {
-        for (var key in selectedDiseaseData.phases) {
-            let p = selectedDiseaseData.phases[key];
-
-            let $backgroundimg;
-            if (p.title == "I") {
-                $imgPath = "images/phases/1.png";
-                $backgroundimg = $imgPath;
-            } else if (p.title == "II") {
-                $imgPath = "images/phases/2.png";
-                $backgroundimg = $imgPath;
-            } else if (p.title == "III") {
-                $imgPath = "images/phases/3.png";
-                $backgroundimg = $imgPath;
-            } else if (p.title == "IV") {
-                $imgPath = "images/phases/4.png";
-                $backgroundimg = $imgPath;
-            }
-
-            $('.projects-container').append(`
+            <input ${a===initialCategory?"checked":""} type="radio" name="yearRadio" value="За всё время" data-disease="${a}" />
+        </label>`)),a===initialCategory)$.each(years.slice(1),function(e,a){t.append($(`<label class="control-btn control-btn-years"> 
+                    ${a}
+                   <input type="radio" name="yearRadio" value="${a}" />
+                </label>`))});else{var i=e.find(function(e){return e.name===a}),n=i?Object.keys(i.phases).map(Number):[];$.each(n,function(e,a){t.append($(`<label class="control-btn control-btn-years"> 
+                    ${a}
+                   <input type="radio" name="yearRadio" value="${a}" />
+                </label>`))})}}function update(e,a){void 0===a&&(a=years[0]),$(".controls.year-buttons .control-btn-years").removeClass("active"),$(`.controls.year-buttons .control-btn-years input[value="${a}"]`).parent().addClass("active"),$(".projects-container").html("");var t=file1.find(function(a){return a.name===e});if("За всё время"===a&&t)for(var i in t.phases){let n=t.phases[i],s;"I"==n.title?s=$imgPath="images/phases/1.png":"II"==n.title?s=$imgPath="images/phases/2.png":"III"==n.title?s=$imgPath="images/phases/3.png":"IV"==n.title&&(s=$imgPath="images/phases/4.png"),$(".projects-container").append(`
                 <article class="project-item">
                     <div class="popup">
                         <figure>
-                            <figcaption style="background: linear-gradient(rgba(85, 133, 181, 0.7), rgba(85, 133, 181, 0.7)), url(${$backgroundimg}); background-position: center; background-size: cover;">
+                            <figcaption style="background: linear-gradient(rgba(85, 133, 181, 0.7), rgba(85, 133, 181, 0.7)), url(${s}); background-position: center; background-size: cover;">
                                 <div class="caption-wrap">
                                     <div class="actions">
-                                        ${p.count === "" ?
-                                            `<h3 class="unhover-info"><b>Фаза: </b>${p.title}</h3>` :
-                                            `<h3 class="unhover-info"><b>Фаза: </b>${p.title}<br>Число исследований: ${p.count}</h3>`
-                                        }
-                                        <h3 class="hover-info"><b>${selectedDisease}<br>${selectedYear}</b></h3>
+                                        ${""===n.count?`<h3 class="unhover-info"><b>Фаза: </b>${n.title}</h3>`:`<h3 class="unhover-info"><b>Фаза: </b>${n.title}<br>Число исследований: ${n.count}</h3>`}
+                                        <h3 class="hover-info"><b>${e}<br>${a}</b></h3>
                                     </div>
                                 </div>
                             </figcaption>
                         </figure>
                     </div>
                 </article>
-            `).hide().fadeIn(200);
-        }
-    } else {
-        // Display blocks based on the selected disease and year
-        var matchingDisease = file2.find(function (item) {
-            return item.phases[selectedYear] && item.name === selectedDisease;
-        });
-
-        var selectedPhases = matchingDisease ? matchingDisease.phases[selectedYear] : file1.find(item => item.name === selectedDisease);
-        var selectedFile = matchingDisease ? 2 : 1;
-
-        if (selectedPhases) {
-            for (var key in selectedPhases) {
-                let p = selectedPhases[key];
-
-                let $backgroundimg;
-                if (p.title == "I") {
-                    $imgPath = "images/phases/1.png";
-                    $backgroundimg = $imgPath;
-                } else if (p.title == "II") {
-                    $imgPath = "images/phases/2.png";
-                    $backgroundimg = $imgPath;
-                } else if (p.title == "III") {
-                    $imgPath = "images/phases/3.png";
-                    $backgroundimg = $imgPath;
-                } else if (p.title == "IV") {
-                    $imgPath = "images/phases/4.png";
-                    $backgroundimg = $imgPath;
-                }
-
-                $('.projects-container').append(`
+            `).hide().fadeIn(200)}else{var o=file2.find(function(t){return t.phases[a]&&t.name===e}),r=o?o.phases[a]:file1.find(a=>a.name===e);if(r)for(var i in r){let l=r[i],c;"I"==l.title?c=$imgPath="images/phases/1.png":"II"==l.title?c=$imgPath="images/phases/2.png":"III"==l.title?c=$imgPath="images/phases/3.png":"IV"==l.title&&(c=$imgPath="images/phases/4.png"),$(".projects-container").append(`
                     <article class="project-item">
                         <div class="popup">
                             <figure>
-                                <figcaption style="background: linear-gradient(rgba(85, 133, 181, 0.7), rgba(85, 133, 181, 0.7)), url(${$backgroundimg}); background-position: center; background-size: cover;">
+                                <figcaption style="background: linear-gradient(rgba(85, 133, 181, 0.7), rgba(85, 133, 181, 0.7)), url(${c}); background-position: center; background-size: cover;">
                                     <div class="caption-wrap">
                                         <div class="actions">
-                                            ${p.count === "" ?
-                                                `<h3 class="unhover-info"><b>Фаза: </b>${p.title}</h3>` :
-                                                `<h3 class="unhover-info"><b>Фаза: </b>${p.title}<br>Число исследований: ${p.count}</h3>`
-                                            }
-                                            <h3 class="hover-info"><b>${selectedDisease}<br>${selectedYear}</b></h3>
+                                            ${""===l.count?`<h3 class="unhover-info"><b>Фаза: </b>${l.title}</h3>`:`<h3 class="unhover-info"><b>Фаза: </b>${l.title}<br>Число исследований: ${l.count}</h3>`}
+                                            <h3 class="hover-info"><b>${e}<br>${a}</b></h3>
                                         </div>
                                     </div>
                                 </figcaption>
                             </figure>
                         </div>
                     </article>
-                `).hide().fadeIn(200);
-            }
-        }
-    }
-}
-
-$(document).ready(function () {
-    populateRadioButtonsForDiseases(file1);
-    populateRadioButtonsForYears(file1, initialCategory);
-    update(initialCategory, initialYear);
-});
-
-$(document).on('change', 'input[name=diseaseRadio]', function () {
-    let selectedDisease = $(this).val();
-    populateRadioButtonsForYears(file2, selectedDisease);
-    update(selectedDisease, $('input[name=yearRadio]:checked').val());
-});
-
-$(document).on('change', 'input[name=yearRadio]', function () {
-    let selectedDisease = $('input[name=diseaseRadio]:checked').val();
-    let selectedYear = $(this).val();
-    update(selectedDisease, selectedYear);
-});
-
-$(document).on('click', '.control-btn-disease', function () {
-    $('.control-btn-disease').removeClass('active');
-    $(this).addClass('active');
-});
-
-$(document).on('click', '.control-btn-years', function () {
-    $('.control-btn-years').removeClass('active');
-    $(this).addClass('active');
-});
-
-// Check if the section is currently visible
-function isSectionVisible() {
-    var section = $('#projects'); // Replace 'projects' with the actual ID of your section
-    var windowHeight = $(window).height();
-    var scrollPos = $(window).scrollTop() || document.documentElement.scrollTop;
-    var sectionOffset = section.offset().top;
-
-    // Adjust the condition to check if the section is at least partially visible
-    return scrollPos + windowHeight > sectionOffset;
-}
-
-function setDefaultValues() {
-    populateRadioButtonsForDiseases(file1);
-    populateRadioButtonsForYears(file1, initialCategory);
-    update(initialCategory, initialYear);
-}
-
-// Add this event listener to handle scrolling
-$(window).scroll(function () {
-    if (!isSectionVisible()) {
-        // If the section is not visible, set default values
-        setDefaultValues();
-    }
-});
-
-
-var isProjectsSectionVisible = true; // Изначально считаем, что секция видима
-
-// Обработчик события прокрутки страницы
-$(window).scroll(function() {
-    // Получение верхней и нижней границ секции "projects"
-    var projectsSectionTop = $('#projects').offset().top;
-    var projectsSectionBottom = projectsSectionTop + $('#projects').outerHeight();
-
-    // Получение верхней и нижней видимой части окна
-    var windowTop = $(window).scrollTop();
-    var windowBottom = windowTop + $(window).height();
-
-    // Проверяем, если секция "projects" не видима
-    if (windowBottom < projectsSectionTop || windowTop > projectsSectionBottom) {
-        // Если она ранее была видима, вызываем функцию update и активируем изначальные кнопки
-        if (isProjectsSectionVisible) {
-            update(initialCategory, initialYear);
-            activateInitialButtons();
-            isProjectsSectionVisible = false;
-        }
-    } else {
-        // Секция "projects" видима
-        isProjectsSectionVisible = true;
-    }
-});
-
-// Функция для активации изначальных кнопок
-function activateInitialButtons() {
-    // Активируем кнопку для заболеваний
-    $('.control-btn-disease').removeClass('active');
-    $('.control-btn-disease:first-child').addClass('active');
-
-    // Активируем кнопку для годов
-    $('controls.year-buttons .control-btn-years').removeClass('active');
-    $('controls.year-buttons .control-btn-years:first-child').addClass('active');
-}
+                `).hide().fadeIn(200)}}}function isSectionVisible(){var e,a=$("#projects"),t=$(window).height();return($(window).scrollTop()||document.documentElement.scrollTop)+t>a.offset().top}function setDefaultValues(){populateRadioButtonsForDiseases(file1),populateRadioButtonsForYears(file1,initialCategory),update(initialCategory,initialYear)}console.log(initialYear),$(document).ready(function(){populateRadioButtonsForDiseases(file1),populateRadioButtonsForYears(file1,initialCategory),update(initialCategory,initialYear)}),$(document).on("change","input[name=diseaseRadio]",function(){let e=$(this).val();populateRadioButtonsForYears(file2,e),update(e,$("input[name=yearRadio]:checked").val())}),$(document).on("change","input[name=yearRadio]",function(){let e=$("input[name=diseaseRadio]:checked").val(),a=$(this).val();update(e,a)}),$(document).on("click",".control-btn-disease",function(){$(".control-btn-disease").removeClass("active"),$(this).addClass("active")}),$(document).on("click",".control-btn-years",function(){$(".control-btn-years").removeClass("active"),$(this).addClass("active")}),$(window).scroll(function(){isSectionVisible()||setDefaultValues()});var isProjectsSectionVisible=!0;function activateInitialButtons(){$(".control-btn-disease").removeClass("active"),$(".control-btn-disease:first-child").addClass("active"),$("controls.year-buttons .control-btn-years").removeClass("active"),$("controls.year-buttons .control-btn-years:first-child").addClass("active")}$(window).scroll(function(){var e=$("#projects").offset().top,a=e+$("#projects").outerHeight(),t=$(window).scrollTop();t+$(window).height()<e||t>a?isProjectsSectionVisible&&(update(initialCategory,initialYear),activateInitialButtons(),isProjectsSectionVisible=!1):isProjectsSectionVisible=!0});
