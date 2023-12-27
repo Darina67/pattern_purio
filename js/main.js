6c5aa1f06431b05478b0d70590307f5e8332df09
Darina67 <user.email=75675517+Darina67@users.noreply.github.com>
2023-12-27 11:34:24 +0300

    upd

--- js/main.js
+++ js/main.js
@@ -122,9 +122,6 @@ jQuery(document).ready(function($) {
 				 $('#side-menu').clone( true ).attr("id", "mmenu-side-menu").mmenu({
 					 position: "right",
 					 zposition:"front",
-					 moveBackground:false,
-					 clone:false,
-					 dragOpen:false,
 					 });
 				 $('#mmenu-side-menu ul').removeClass('nav navbar-nav navbar-right');
 			 }
